28bb5b8c719d76dc79d794a759411b20278aa989
marleigh <volcana@gmail.com>
2021-12-16 12:25:34 -0800

    learned the basics of forms in react

--- src/components/Start.js
+++ src/components/Start.js
@@ -3,22 +3,17 @@ import Conversation from './Conversation';
 
 export default function Start() {
 
-    const charNameInput = document.getElementById('char-name');
-    let charName = '';
     const [character, setCharacter] = React.useState('');
 
-    function handleSubmit() {
-        
-        charName = charNameInput.value;
-        setCharacter(charName);
-
-        //document.querySelector('.start-input').style.display = "none";
+    function handleSubmit(event) { 
+        setCharacter(event.target.value);
+        console.log(character);
     }   
 
     return(
         <div className="start-input">
-            <input type="text" placeholder="Character (not Player) Name" id="char-name" className="input-text"/>
-            <button id="char-name-submit" className="btn" onClick={handleSubmit}>Submit</button>
+            <input type="text" placeholder="Character (not Player) Name" id="char-name" className="input-text" onChange={handleSubmit}/>
+            {/* <button id="char-name-submit" className="btn" onClick={handleSubmit}>Submit</button> */}
             <Conversation character={character}/> {/*char getting passed in before the event fires*/ }
         </div>
     )
