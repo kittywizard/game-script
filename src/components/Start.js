1ebb64eb13b9c79b7ba92d69e8f640942ee63dc5
marleigh <volcana@gmail.com>
2021-12-17 10:36:00 -0800

    object in state

--- src/components/Start.js
+++ src/components/Start.js
@@ -3,18 +3,32 @@ import Conversation from './Conversation';
 
 export default function Start() {
 
-    const [character, setCharacter] = React.useState('');
+    const [character, setCharacter] = React.useState(
+                                            {"characterName": "",
+                                            "otherData": ""});
 
     function handleSubmit(event) { 
-        setCharacter(event.target.value);
-        console.log(character);
+        setCharacter(prevState => {
+            return {
+                ...prevState, //use the spread operator to take previous state and then add to it
+                [event.target.name]: event.target.value
+            }
+            
+        });
     }   
 
     return(
         <div className="start-input">
-            <input type="text" placeholder="Character (not Player) Name" id="char-name" className="input-text" onChange={handleSubmit}/>
+            <input type="text" 
+                placeholder="Character (not Player) Name" 
+                id="char-name" 
+                className="input-text"
+                name="characterName" 
+                onChange={handleSubmit}
+            />
+
             {/* <button id="char-name-submit" className="btn" onClick={handleSubmit}>Submit</button> */}
-            <Conversation character={character}/> {/*char getting passed in before the event fires*/ }
+            {/* <Conversation character={character}/> {/*char getting passed in before the event fires*/ }
         </div>
     )
 }
